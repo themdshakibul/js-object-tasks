11589f94a035fe26da216e20b621509e1a7db2e5
themdshakibul <mdshakibul88240@gmail.com>
2026-01-28 15:36:06 +0600

    Task 2 completed

--- js-object.js
+++ js-object.js
@@ -2,10 +2,22 @@
 // Access the `golden rod` color value in output.
 
 const colors = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520'
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+  "golden rod": "#daa520",
 };
 
-console.log(colors["golden rod"])
+console.log(colors["golden rod"]);
+
+// ### Task-2
+// For this object below add a property named `passenger capacity` with value 5
+
+const car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2020,
+};
+
+car["passenger capacity"] = 5;
+console.log(car);
