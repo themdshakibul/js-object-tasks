c707332b5928e5c2d071620f601867573a6726c9
themdshakibul <mdshakibul88240@gmail.com>
2026-01-28 15:40:40 +0600

    Task 3 completed

--- js-object.js
+++ js-object.js
@@ -8,7 +8,7 @@ const colors = {
   "golden rod": "#daa520",
 };
 
-console.log(colors["golden rod"]);
+// console.log(colors["golden rod"]);
 
 // ### Task-2
 // For this object below add a property named `passenger capacity` with value 5
@@ -20,4 +20,19 @@ const car = {
 };
 
 car["passenger capacity"] = 5;
-console.log(car);
+// console.log(car);
+
+// ### Task-3
+// Display the physics marks as output.
+
+const student = {
+  name: "Hamim Sakep",
+  id: 5421,
+  physics: {
+    subject: "HSC Physics",
+    author: "Shahjahan Tapan",
+    marks: 30,
+  },
+};
+
+console.log(student.physics.marks);
